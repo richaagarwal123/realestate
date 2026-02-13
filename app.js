ae4b5d187a01a6ed8410f9f25df1bac8f1c95cf2
richaagarwal123 <165362283+richaagarwal123@users.noreply.github.com>
2026-02-13 19:12:49 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -80,16 +80,23 @@ var swiper = new Swiper(".ttab ", {
 
 });
 
-let hmenu=document.querySelector(".h-menu")
 
 
-function toggleMenu(){
-    if(hmenu.style.display=="flex")
-    {
-       hmenu.style.display="none"
-    }
-    else{
-        hmenu.style.display="flex"
-    }
+
+const bar= document.getElementById('bar');
+const close= document.getElementById('close');
+const nav=document.querySelector('.h-menu');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+
+    })
 }
 
+if(close){
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+
+    })
+}
